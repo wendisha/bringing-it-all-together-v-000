267554e7d615eab746963685535cb3e8ac9ba30e
Wendy Calderon <wendycalderon@gmail.com>
2018-09-06 22:55:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,4 +77,9 @@ class Dog
       self.new_from_db(row)
     end.first
   end
+  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
