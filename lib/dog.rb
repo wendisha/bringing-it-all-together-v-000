464b3a21212f20f92972755c64e04de1d0e312ed
Wendy Calderon <wendycalderon@gmail.com>
2018-09-06 20:59:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Dog 
   attr_accessor :id, :name, :breed
-  def initialize(id: = nil, name:, breed:)
+  def initialize(id: nil, name:, breed:)
     @id = id
     @name = name 
     @breed = breed
